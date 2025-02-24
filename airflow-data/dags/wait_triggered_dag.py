86d52ed2f93c70f01a2fd433e5913794691f6f6d
Avik Mandal <avik.burdwan@gmail.com>
2025-02-24 17:26:39 +0530

    updated trigger dags steps

--- airflow-data/dags/wait_triggered_dag.py
+++ airflow-data/dags/wait_triggered_dag.py
@@ -16,22 +16,34 @@ with DAG(
       "example=triggered_dag",
     ],
 ) as dag:
-
-  trigger = TriggerDagRunOperator(
-    task_id='trigger_target_dag',
-    trigger_dag_id='trigger_target_dag',  # Replace with the actual DAG ID you want to trigger
-    conf={'key': 'value'},  # Optional: Configuration dictionary to pass to the triggered DAG
-    wait_for_completion=True,  # Wait for the triggered DAG to complete
-    allowed_states=['success'],  # Continue only if the triggered DAG succeeds
-    failed_states=['failed'],  # Fail if the triggered DAG fails or is skipped
-    poke_interval=10,  # Check the status every 60 seconds
-  )
+    config = [
+        {'key': 'value11', "key2": "value21"},
+        {'key': 'value12', "key2": "value22"},
+        {'key': 'value13', "key2": "value23"},
+    ]
+  
+    # Define the TriggerDagRunOperator to trigger the target DAG having each value for config
+    previous_task = None
+    for i, conf in enumerate(config):
+        trigger = TriggerDagRunOperator(
+            task_id=f'trigger_target_dag_{conf.get("key")}',
+            trigger_dag_id='trigger_target_dag',  # Replace with the actual DAG ID you want to trigger
+            conf=conf,  # Configuration dictionary to pass to the triggered DAG
+            wait_for_completion=True,  # Wait for the triggered DAG to complete
+            allowed_states=['success'],  # Continue only if the triggered DAG succeeds
+            failed_states=['failed'],  # Fail if the triggered DAG fails
+            poke_interval=10,  # Check the status every 10 seconds
+        )
+        
+        if previous_task:
+            previous_task >> trigger
+        previous_task = trigger
     
-  some_other_task = EmptyOperator(
-    task_id='some-other-task',
-  )
+    some_other_task = EmptyOperator(
+        task_id='some-other-task',
+    )
     
-  trigger >> some_other_task
+    previous_task >> some_other_task
 
 
   
